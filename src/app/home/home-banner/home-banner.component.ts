ad813f0bfe73a1842f6ffecb194b271c8d766050
aditya-indus <aditya@indusos.com>
2021-05-18 11:15:12 +0530

    banner parallax created.

--- src/app/home/home-banner/home-banner.component.ts
+++ src/app/home/home-banner/home-banner.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import tilt from 'vanilla-tilt';
+import Rellax from 'rellax';
 @Component({
   selector: 'app-home-banner',
   templateUrl: './home-banner.component.html',
   styleUrls: ['./home-banner.component.scss']
 })
-export class HomeBannerComponent implements OnInit {
+export class HomeBannerComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() {
+    let nodeArray = [];
+    document.querySelectorAll('#banner-holder').forEach(elem => {
+      nodeArray.push(elem);
+    })
+    tilt.init(nodeArray, {
+      max: 25,
+      speed: 400
+    });
 
+    var rellax = new Rellax('.rellax');
+  }
 }
